48ba8298a664399bfe2584a4dd7325c04cc3efdc
Alex Jones <alexander.jones@softwire.com>
2020-09-09 22:42:01 +0100

    FIrst working cloud example (without credentials)

--- setup_gmod.sh
+++ setup_gmod.sh
@@ -7,14 +7,10 @@ if [ ! -d ~/gmodds/cstrike ]
 then
     ~/steamcmd/steamcmd.sh +login anonymous +force_install_dir ../gmodds/cstrike +app_update 232330 validate +quit
 fi
-if [ ! -d ~/gmodds/csgo ]
-then
-    ~/steamcmd/steamcmd.sh +login anonymous +force_install_dir ../gmodds/csgo +app_update 740 validate +quit
-fi
 
 cp ~/mount.cfg ~/gmodds/garrysmod/cfg
 cp ~/server.cfg ~/gmodds/garrysmod/cfg
 sed -i "s/sv_password/sv_password ${SERVER_PASSWORD}/" ~/gmodds/garrysmod/cfg/server.cfg
 sed -i "s/sv_password/rcon_password ${RCON_PASSWORD}/" ~/gmodds/garrysmod/cfg/server.cfg
 
-~/gmodds/srcds_run -game garrysmod +gamemode prop_hunt +maxplayers 32 +map gm_construct +host_workshop_collection 2215505010 -ip 0.0.0.0 -port 27016 -condebug
+~/gmodds/srcds_run -game garrysmod +gamemode prop_hunt +maxplayers 32 +map gm_construct +host_workshop_collection 2215505010 -ip 0.0.0.0 -port ${SERVER_PORT} -condebug
