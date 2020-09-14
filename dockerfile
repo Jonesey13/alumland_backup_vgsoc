f41db4d5030582a30fb09e3f0a9fb47c562483c9
Alex Jones <alexander.jones@softwire.com>
2020-09-14 21:29:40 +0100

    Fix CStrike

--- dockerfile
+++ dockerfile
@@ -1,7 +1,8 @@
 FROM cm2network/steamcmd
+USER root
+RUN apt-get update && apt-get install -y git
+USER steam
 COPY . /home/steam
 RUN mkdir /home/steam/gmodds && chown steam /home/steam/gmodds
 ENTRYPOINT /home/steam/setup_gmod.sh
 EXPOSE 27015
-EXPOSE 27016
-EXPOSE 27005
--- setup_gmod.sh
+++ setup_gmod.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
+if [ ! -d ~/gmodds/css/cstrike ]
+then
+    mkdir ~/gmodds/css
+    ~/steamcmd/steamcmd.sh +login anonymous +force_install_dir ../gmodds/css +app_update 232330 validate +quit
+fi
 if [ ! -f ~/gmodds/srcds_run ]
 then
     ~/steamcmd/steamcmd.sh +login anonymous +force_install_dir ../gmodds +app_update 4020 validate +quit
 fi
-if [ ! -d ~/gmodds/cstrike ]
+if [ ! -d ~/gmodds/garrysmod/gamemodes/prop_hunt ]
 then
-    ~/steamcmd/steamcmd.sh +login anonymous +force_install_dir ../gmodds/cstrike +app_update 232330 validate +quit
+    pushd ~/gmodds/garrysmod/gamemodes
+    git clone https://github.com/Newbrict/ObjHunt.git prop_hunt
+    popd
 fi
 
 cp ~/mount.cfg ~/gmodds/garrysmod/cfg
 cp ~/server.cfg ~/gmodds/garrysmod/cfg
 sed -i "s/sv_password/sv_password ${SERVER_PASSWORD}/" ~/gmodds/garrysmod/cfg/server.cfg
-sed -i "s/sv_password/rcon_password ${RCON_PASSWORD}/" ~/gmodds/garrysmod/cfg/server.cfg
+sed -i "s/rcon_password/rcon_password ${RCON_PASSWORD}/" ~/gmodds/garrysmod/cfg/server.cfg
 
-~/gmodds/srcds_run -game garrysmod +gamemode prop_hunt +maxplayers 32 +map gm_construct +host_workshop_collection 2215505010 -ip 0.0.0.0 -port ${SERVER_PORT} -condebug
+~/gmodds/srcds_run -game garrysmod +gamemode prop_hunt +maxplayers 32 +map cs_italy +host_workshop_collection 2215505010 -ip 0.0.0.0 -port ${SERVER_PORT:=27015} -condebug
